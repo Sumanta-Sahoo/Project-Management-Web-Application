44afc46a240f7c2bbddffed352b42d810cfec62a
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-07-22 12:45:55 +0530

    Implement all model/entity classes with properties(Chat, Comment, Message, User, Issue, Project)

--- src/main/java/com/SumPortfolio/Model/Issue.java
+++ src/main/java/com/SumPortfolio/Model/Issue.java
@@ -1,7 +1,12 @@
 package com.SumPortfolio.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -9,7 +14,20 @@ public class Issue {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String issueTitle;
+    private String issueDescription;
+    private String issueStatus;
+    private Long projectId;
+    private String issuePriority;
+    private LocalDate dueDate;
+    private List<String> tags = new ArrayList<>();
 
     @ManyToOne
     private User assignee;
+    @JsonIgnore
+    @ManyToOne
+    private Project project;
+    @JsonIgnore
+    @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
