daea97d25b314958ee06c5aa4fd19d13379a78d3
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-07-24 18:55:32 +0530

    fix annotations in implementation class (CommentServiceImpl, EmailServiceImpl)

--- src/main/java/com/SumPortfolio/Service/CommentServiceImpl.java
+++ src/main/java/com/SumPortfolio/Service/CommentServiceImpl.java
@@ -7,11 +7,13 @@ import com.SumPortfolio.Repository.CommentRepository;
 import com.SumPortfolio.Repository.IssueRepository;
 import com.SumPortfolio.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CommentServiceImpl implements ICommentService{
 
     @Autowired
